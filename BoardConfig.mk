c3dc7a02aca3facaf618b752199b7dade23b987f
zskynet09 <hakimfbacc@gmail.com>
2021-09-02 22:36:54 +0530

    PNX_sprout: Set screen brightness path & default brightness

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,9 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 102
 
 # Debug flags
 TWRP_INCLUDE_LOGCAT := true
