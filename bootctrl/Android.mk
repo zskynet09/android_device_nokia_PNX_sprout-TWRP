237d3548df1f132046d031f7ea5f24a57d04d8b9
HS <78603446+zskynet09@users.noreply.github.com>
2021-09-15 16:43:52 +0800

    PNX_sprout: Update makefile bootctrl according to CAF
    Tags: LA.UM.8.8.r1-05100-SDM710.0

--- bootctrl/Android.mk
+++ bootctrl/Android.mk
@@ -1,17 +1,17 @@
+ifeq ($(AB_OTA_UPDATER),true)
 LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
 
 # HAL Shared library for the target. Used by libhardware.
-include $(CLEAR_VARS)
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/gpt-utils/inc
 LOCAL_CFLAGS += -Wall -Werror
 LOCAL_SHARED_LIBRARIES += liblog libgptutils libcutils
-LOCAL_HEADER_LIBRARIES := libhardware_headers libsystem_headers
+LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_SRC_FILES := boot_control.cpp
-LOCAL_MODULE_PATH_64 := $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := bootctrl.sdm710
-LOCAL_MODULE_OWNER := qcom
-LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_OWNER := qti
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 # Static library for the target. Used by update_engine_sideload from recovery.
@@ -19,9 +19,10 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/gpt-utils/inc
 LOCAL_CFLAGS += -Wall -Werror
 LOCAL_SHARED_LIBRARIES += liblog libgptutils libcutils
-LOCAL_HEADER_LIBRARIES := libhardware_headers libsystem_headers
+LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_SRC_FILES := boot_control.cpp
-LOCAL_MODULE_PATH_64 := $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := bootctrl.sdm710
 include $(BUILD_STATIC_LIBRARY)
+
+endif
