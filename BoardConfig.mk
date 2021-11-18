9a57610a1ffccc7b2e302f47a31fb90d1641baa1
Aseem Athale <athaleaseem@gmail.com>
2021-11-18 13:55:37 +0530

    PNX_sprout: Remove TW_USE_TOOLBOX Toolbox has long been the default in TWRP, this flag is no longer needed or used. In fact, it is automatically assumed true, unless you specifically specify TW_FORCE_USE_BUSYBOX.
    Change-Id: If19d67f9377481e522a488364bd881c5fb389611

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,7 +112,6 @@ TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 128
